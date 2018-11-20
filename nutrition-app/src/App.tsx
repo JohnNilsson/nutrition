6f3d9b96145e599062b92c00577282f92c5a991e
John Nilsson <john@milsson.nu>
2018-11-20 23:55:28 +0100

    A first dump of the nutrition database

--- nutrition-app/src/App.tsx
+++ nutrition-app/src/App.tsx
@@ -1,11 +1,65 @@
 import * as React from "react";
 
 import TopMenu from "./components/TopMenu";
-import NutritionStats from "./components/NurtitionStats";
-import SelectedFoods from "./components/SelectedFoods";
+// import NutritionStats from "./components/NurtitionStats";
+// import SelectedFoods from "./components/SelectedFoods";
+
+import { AutoSizer, Grid, GridCellProps } from 'react-virtualized'
+import {Naringsvarde} from "swedish-food-composition-database/src/XmlTo6NFTransform";
+
+class NutritionTable extends React.Component<{},{db:Naringsvarde|null}>
+{
+  constructor(props: {})
+  {
+    super(props);
+    this.state = {db:null};
+    this.loadDb().then(db => {
+      this.setState({db});
+    });
+  }
+
+  private async loadDb()
+  {
+    const db = await import("swedish-food-composition-database/data/Naringsvarde.6NF.json");
+    console.log(db.default);
+    return db.default;
+  }
+
+  render() {
+    const {db} = this.state;
+    return (db === null)
+    ? <div />
+    : <AutoSizer>
+      {({ height, width }) => (
+        <Grid
+          cellRenderer={({ rowIndex, columnIndex, key, style }) =>
+          <div key={key} style={style}>{
+            rowIndex == 0
+            ? (columnIndex === 0 ? "id" :
+               columnIndex === 1 ? "name" :
+               db.Naringsamne.Namn[columnIndex-2])
+            : (columnIndex === 0 ? db.Livsmedel.Nummer[rowIndex-1] :
+               columnIndex === 1 ? db.Livsmedel.Namn[rowIndex-1] :
+               db.Livsmedel.Naringsvarde[db.Naringsamne.Forkortning[columnIndex-2]].Varde[rowIndex-1])
+          }</div>}
+          columnCount={db.Naringsamne.Forkortning.length + 2}
+          columnWidth={100}
+          height={height}
+          rowCount={db.Livsmedel.Nummer.length + 1}
+          rowHeight={50}
+          width={width}
+        />
+      )}
+      </AutoSizer>;
+  }
+}
+
+
+
 
 import "semantic-ui-css/semantic.min.css";
 import { FlexDirectionProperty } from "csstype";
+import { Component } from "react";
 
 const appStyle = {
   height: "100vh"
@@ -35,10 +89,9 @@ const App = () => (
       <TopMenu />
       <FlexCol>
         <FlexColItem>
-          <SelectedFoods />
-        </FlexColItem>
-        <FlexColItem>
-          <NutritionStats />
+          <NutritionTable />
+          {/* <SelectedFoods /> */}
+          {/* <NutritionStats /> */}
         </FlexColItem>
       </FlexCol>
     </React.StrictMode>
