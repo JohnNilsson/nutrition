1e849ddfda6c7b9ffd1850305cc0bc3945c26ab1
John Nilsson <john@milsson.nu>
2021-04-25 11:17:25 +0200

    Make jest play nice with es-modules

--- nutrition-app/src/App.test.tsx
+++ nutrition-app/src/App.test.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import { AppState } from "./state";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  const state = new AppState();
+  ReactDOM.render(<App state={state} />, div);
 });
